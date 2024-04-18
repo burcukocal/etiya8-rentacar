c1c595730f64384aa435c5d9f7ccfa7a09a3bc97
burcukocal <burcukocal97@gmail.com>
2024-04-18 20:55:26 +0300

    add project

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MainLayoutComponent } from '../../layouts/main-layout/main-layout.component';
+
+export type NavbarItem = {
+  label: string;
+  routerLink: string;
+};
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule, MainLayoutComponent
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {
+  @Input() items: NavbarItem[] = [];
+}
