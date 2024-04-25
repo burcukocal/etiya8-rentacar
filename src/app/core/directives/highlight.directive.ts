13a1c10ff9e3e3cd724a26d33b6cde6ef1ac6e0e
burcukocal <burcukocal97@gmail.com>
2024-04-25 10:46:32 +0300

    add directive

---
+++ src/app/core/directives/highlight.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+  standalone: true,
+})
+export class HighlightDirective implements OnInit {
+  @Input("appHighlight") bgColor: string = 'yellow';
+  @Input("appHighlightThen") secondBgColor: string = 'yellow';
+  @Input() color = 'black';
+  //input kullanılacaksa constructorda değil ngOnInit içinde yapılır.
+
+  constructor(private element: ElementRef<HTMLElement>) {}
+
+  ngOnInit() {
+    const span = document.createElement('span');
+    span.style.backgroundColor ='yellow';
+    span.style.color = 'black';
+    span.innerText = 'On Sale! ';
+
+    this.element.nativeElement.appendChild(span);
+
+    this.element.nativeElement.style.color=this.color
+    this.element.nativeElement.style.backgroundColor = this.bgColor;
+  }
+}
