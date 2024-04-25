45f471227e65043bb5d38cf799ce82317ef3d8f2
burcukocal <burcukocal97@gmail.com>
2024-04-25 11:14:52 +0300

    add event

--- src/app/core/directives/highlight.directive.ts
+++ src/app/core/directives/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
@@ -16,11 +16,25 @@ export class HighlightDirective implements OnInit {
     const span = document.createElement('span');
     span.style.backgroundColor ='yellow';
     span.style.color = 'black';
-    span.innerText = 'On Sale! ';
+    span.innerText = ' On Sale!';
 
     this.element.nativeElement.appendChild(span);
 
     this.element.nativeElement.style.color=this.color
+    this.element.nativeElement.style.backgroundColor = this.bgColor;
+
+    this.element.nativeElement.onclick = () => {
+      alert("You clicked me!")
+    }
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter(){
+    this.element.nativeElement.style.backgroundColor = 'lightblue'
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave(){
     this.element.nativeElement.style.backgroundColor = this.bgColor;
   }
 }
