0e5d6b5a7c50b32917657d98e491c0960572abc4
burcukocal <burcukocal97@gmail.com>
2024-04-26 14:16:15 +0300

    update

--- src/app/features/customers/components/customer-list/customer-list.component.ts
+++ src/app/features/customers/components/customer-list/customer-list.component.ts
@@ -2,12 +2,16 @@ import { CustomersApiService } from './../../services/customersApi.service';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerListItemDto } from '../../models/customer-list-item-dto';
+import { WelcomePageComponent } from '../../../../routers/welcome-page/welcome-page.component';
+import { WelcomeDirective } from '../../../../core/directives/welcome.directive';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
   imports: [
     CommonModule,
+    WelcomePageComponent,
+    WelcomeDirective
   ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss',
@@ -22,8 +26,16 @@ export class CustomerListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getList();
+    //this.getList();
+    this.getListWithDelay();
   }
+
+  getListWithDelay(){
+    setTimeout(() => {
+      this.getList();
+    }, 3000);
+  }
+
   getList() {
     this.customersApiService.getList().subscribe(customers => {
       this.list = customers;
